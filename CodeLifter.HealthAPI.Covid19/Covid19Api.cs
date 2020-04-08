b0b47e6988ab2d8f8678eb0911abd574ef79fe2c
Andrew Palmer <a@codelifter.net>
2020-04-08 10:35:21 -0700

    pdating package and nuget dependencies

--- CodeLifter.HealthAPI.Covid19/Covid19Api.cs
+++ CodeLifter.HealthAPI.Covid19/Covid19Api.cs
@@ -25,7 +25,7 @@ namespace CodeLifter.HealthAPI.Covid19
         public Covid19Api()
         {
             BaseURI = "https://health-api.com/api/v1/";
-            Client = new HttpClient(BaseURI, true, true);
+            Client = new HttpClient(BaseURI);
         }
 
         public Covid19Api(HttpClient client)
@@ -36,35 +36,35 @@ namespace CodeLifter.HealthAPI.Covid19
         public async Task<Statistic> GetGlobalStatistics()
         {
             HttpRequest request = new HttpRequest("covid-19/total");
-            var response = await Client.Get<Statistic>(request);
+            var response = await Client.GetFromCache<Statistic>(request, $"get-global-statistics");
             return response;
         }
 
         public async Task<List<State>> GetAllStates()
         {
             HttpRequest request = new HttpRequest("covid-19/all");
-            var response = await Client.Get<List<State>>(request);
+            var response = await Client.GetFromCache<List<State>>(request, $"get-global-states");
             return response;
         }
 
         public async Task<Country> GetCountry(string countryCode)
         {
             HttpRequest request = new HttpRequest($"covid-19/{countryCode}");
-            var response = await Client.Get<Country>(request);
+            var response = await Client.GetFromCache<Country>(request, $"get-data-for-country-{countryCode}");
             return response;
         }
 
         public async Task<List<State>> GetStatesByCountry(string countryCode)
         {
             HttpRequest request = new HttpRequest($"covid-19/{countryCode}/full");
-            var response = await Client.Get<List<State>>(request);
+            var response = await Client.GetFromCache<List<State>>(request, $"get-all-state-data-for-country-{countryCode}");
             return response;
         }
 
         public async Task<List<Country>> GetAllCountries()
         {
             HttpRequest request = new HttpRequest("covid-19/countries");
-            var response = await Client.Get<List<Country>>(request);
+            var response = await Client.GetFromCache<List<Country>>(request, $"get-all-countries");
             return response;
         }
     }
